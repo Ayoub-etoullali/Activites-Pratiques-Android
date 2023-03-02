5b4b8c4d3ed61b00d844559ff43ffb5db183f435
Ayoub ETOULLALI <ayoub.etoullali2002@example.com>
2023-03-03 00:37:34 +0100

    TP2

--- TP2 - Splash screen, Navigation entre Ecrans, Internationalisation/code/app/src/main/java/ma/enset/myapplication/SplashActivity.java
+++ TP2 - Splash screen, Navigation entre Ecrans, Internationalisation/code/app/src/main/java/ma/enset/myapplication/SplashActivity.java
@@ -1,20 +1,31 @@
 package ma.enset.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 
 public class SplashActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
-/*
-    private static int SPLASH_TIME_OUT = 10000;
-    new Handler().ostDelayed(new Runnable()){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
 
     }
- */
 }
